5ec004d472d8f8a0414880aeecda8f4ba1e7b997
João Paulo R.S <joaopauloit@gmail.com>
2024-09-25 00:19:41 -0300

    feat() - continuando módulo admin

--- src/front-end/src/componentes/ListaRestaurantes/index.tsx
+++ src/front-end/src/componentes/ListaRestaurantes/index.tsx
@@ -8,28 +8,20 @@ import { IPaginacao } from '../../interfaces/IPaginacao';
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
+  const [paginaAnterior, setPaginaAnterior] = useState('');
 
   useEffect(() => {
     //Obter restaurantes
+    carregarDados('http://localhost:8000/api/v1/restaurantes/');
+  }, []);
+
+  const carregarDados = (url: string) => {
     axios
-      .get<IPaginacao<IRestaurante>>(
-        'http://localhost:8000/api/v1/restaurantes/'
-      )
+      .get<IPaginacao<IRestaurante>>(url)
       .then((resposta) => {
         setRestaurantes(resposta.data.results);
         setProximaPagina(resposta.data.next);
-      })
-      .catch((erro) => {
-        console.log(erro);
-      });
-  }, []);
-
-  const verMais = () => {
-    axios
-      .get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then((resposta) => {
-        setRestaurantes([...restaurantes, ...resposta.data.results]);
-        setProximaPagina(resposta.data.next);
+        setPaginaAnterior(resposta.data.previous);
       })
       .catch((erro) => {
         console.log(erro);
@@ -44,7 +36,22 @@ const ListaRestaurantes = () => {
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      {proximaPagina && <button onClick={() => verMais()}>ver mais</button>}
+      {
+        <button
+          onClick={() => carregarDados(paginaAnterior)}
+          disabled={!paginaAnterior}
+        >
+          Página Anterior
+        </button>
+      }
+      {
+        <button
+          onClick={() => carregarDados(proximaPagina)}
+          disabled={!proximaPagina}
+        >
+          Próxima página
+        </button>
+      }
     </section>
   );
 };
