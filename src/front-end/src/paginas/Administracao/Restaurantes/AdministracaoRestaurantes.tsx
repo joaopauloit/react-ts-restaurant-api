61a5a2a4a657d75bea161701e0a924321a50a185
João Paulo R.S <joaopauloit@gmail.com>
2024-09-26 23:55:27 -0300

    feat() - pesquisa

--- src/front-end/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/front-end/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  FormControl,
   Paper,
   Table,
   TableBody,
@@ -7,6 +8,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -15,6 +17,7 @@ import { Link } from 'react-router-dom';
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [search, setSearch] = useState('');
   useEffect(() => {
     axios
       .get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
@@ -26,50 +29,88 @@ const AdministracaoRestaurantes = () => {
 
   const excluir = (restauranteExcluir: IRestaurante) => {
     axios
-      .delete(`http://localhost:8000/api/v2/restaurantes/${restauranteExcluir.id}/`)
+      .delete(
+        `http://localhost:8000/api/v2/restaurantes/${restauranteExcluir.id}/`
+      )
       .then((resposta) => {
-        alert(`Restaurante "${restauranteExcluir.nome}" excluido com sucesso!`)
-        const listaRestaurate = restaurantes.filter(restaurante => restaurante.id !== restauranteExcluir.id);
+        alert(`Restaurante "${restauranteExcluir.nome}" excluido com sucesso!`);
+        const listaRestaurate = restaurantes.filter(
+          (restaurante) => restaurante.id !== restauranteExcluir.id
+        );
         setRestaurantes([...listaRestaurate]);
       })
       .catch((err) => {
-        alert('Ocorreu um erro inesperado ao deletar')
+        alert('Ocorreu um erro inesperado ao deletar');
       });
   };
 
+  const pesquisarRestaurantes = () => {
+    axios
+      .get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/', {
+        params: {
+          ordering: 'nome',
+          search,
+        },
+      })
+      .then((resposta) => {
+        setRestaurantes(resposta.data);
+      })
+      .catch((err) => {});
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell>Editar</TableCell>
-            <TableCell>Excluir</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {restaurantes.map((restaurante) => (
-            <TableRow key={restaurante.id}>
-              <TableCell>{restaurante.nome}</TableCell>
-              <TableCell>
-                [
-                <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>
-                ]
-              </TableCell>
-              <TableCell>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => excluir(restaurante)}
-                >
-                  Excluir
-                </Button>
-              </TableCell>
+    <>
+      <FormControl>
+        <TextField
+          value={search}
+          onChange={(evento) => setSearch(evento.target.value)}
+          id="standard-basic"
+          label="Nome do Restaurante"
+          variant="standard"
+        />
+        <Button
+          type="submit"
+          variant="outlined"
+          onClick={() => pesquisarRestaurantes()}
+        >
+          Pesquisar
+        </Button>
+      </FormControl>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>Editar</TableCell>
+              <TableCell>Excluir</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {restaurantes.map((restaurante) => (
+              <TableRow key={restaurante.id}>
+                <TableCell>{restaurante.nome}</TableCell>
+                <TableCell>
+                  [
+                  <Link to={`/admin/restaurantes/${restaurante.id}`}>
+                    Editar
+                  </Link>
+                  ]
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => excluir(restaurante)}
+                  >
+                    Excluir
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
